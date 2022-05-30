b9f56c6f5507ba69e210dae423c605bc58828352
Rohan Mukherjee <rbmuk@outlook.com>
2022-05-29 22:21:30 -0700

    Fixed time schedule on class

--- class_skeleton.py
+++ class_skeleton.py
@@ -1,22 +1,3 @@
-def format_string(s: str) -> str:
-    s = s[:-3]
-    if (len(s) < 5):
-        s = "0" + s
-    return s
-
-def get_times(quarter):
-    times = { "Monday": {"start": "00:00", "end": "00:00"}, 
-             "Tuesday": {"start": "00:00", "end": "00:00"}, 
-             "Wednesday": {"start": "00:00", "end": "00:00"}, 
-             "Thursday": {"start": "00:00", "end": "00:00"}, 
-             "Friday": {"start": "00:00", "end": "00:00"} }
-    for i in range(len(quarter)):
-        currTimes = quarter[i].find_elements_by_xpath(".//tr[1]/td[4]/div/div")
-        for j in range(len(currTimes)):
-            days = currTimes[j].find_element_by_xpath(".//div[1]/span").get_attribute("aria-label")
-            print(days)
-            print("-----------------------" + "\n")
-
 class Class:
     times = { "Monday": {"start": "00:00", "end": "00:00"}, 
              "Tuesday": {"start": "00:00", "end": "00:00"}, 
@@ -26,31 +7,53 @@ class Class:
     timeStart = None
     timeEnd = None
     SLN = None
-    def __init__(self, timeStart: str, timeEnd: str, SLN: int):
-        self.timeStart = timeStart
-        self.timeEnd = timeEnd
+    def __init__(self, times: dict, SLN: int):
+        self.times = times
         self.SLN = SLN
 
+def get_times(section) -> dict:
+    times = { "Monday": {"start": "00:00", "end": "00:00"}, 
+             "Tuesday": {"start": "00:00", "end": "00:00"}, 
+             "Wednesday": {"start": "00:00", "end": "00:00"}, 
+             "Thursday": {"start": "00:00", "end": "00:00"}, 
+             "Friday": {"start": "00:00", "end": "00:00"} }
+    week = section.find_elements_by_xpath(".//tr[1]/td[4]/div/div")
+    for x in week:
+        days = x.find_element_by_xpath(".//div[1]/span").get_attribute("aria-label").split(", ")
+        timez = x.find_elements_by_xpath(".//div[2]/time")
+        for day in days:
+            times[day] = {"start": timez[0].get_attribute("datetime"), "end": timez[1].get_attribute("datetime")}
+    return times
+
+def ret_times(times: dict) -> str:
+    ret = ""
+    if times["Monday"]["start"] != "00:00":
+        ret += "Monday: " + times["Monday"]["start"] + "-" + times["Monday"]["end"] + "\n"
+    if times["Tuesday"]["start"] != "00:00":
+        ret += "Tuesday: " + times["Tuesday"]["start"] + "-" + times["Tuesday"]["end"] + "\n"
+    if times["Wednesday"]["start"] != "00:00":
+        ret += "Wednesday: " + times["Wednesday"]["start"] + "-" + times["Wednesday"]["end"] + "\n"
+    if times["Thursday"]["start"] != "00:00":
+        ret += "Thursday: " + times["Thursday"]["start"] + "-" + times["Thursday"]["end"] + "\n"
+    if times["Friday"]["start"] != "00:00":
+        ret += "Friday: " + times["Friday"]["start"] + "-" + times["Friday"]["end"] + "\n"
+    return ret
 
 class ClassList:
     classlist = []
     name = None
 
-
-    def __init__(self, name, classes):
+    def __init__(self, name, quarter):
         self.name = name
         classlist = []
-        for i in range(len(classes)):
-            times = classes[i].find_elements_by_xpath(".//tr[1]/td[4]/div/div/div[2]/time")
-            timeStart = format_string(times[0].text)
-            timeEnd = format_string(times[1].text)
-            get_times(classes)
-            SLN = classes[i].find_element_by_xpath(".//tr[1]/td[6]").text[-5:]
-            self.classlist.append(Class(timeStart, timeEnd, SLN))
+        for section in quarter:
+            times = get_times(section)
+            SLN = section.find_element_by_xpath(".//tr[1]/td[6]").text[-5:]
+            self.classlist.append(Class(times, SLN))
 
     def to_string(self) -> str:
         class_info = ""
         for x in self.classlist:
-            class_info += self.name + " goes from " + x.timeStart + " to " + x.timeEnd + ", SLN: " + x.SLN + "\n"
+            class_info += self.name + ":\n" + ret_times(x.times) + "SLN: " + x.SLN + "\n\n"
         return class_info
 
